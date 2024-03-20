c1ff99769cbdd9d0d1b4a2df7275fea00b0f5e4b
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-03-20 19:28:15 +0200

    fix DB schema

--- school_management.sql
+++ school_management.sql
@@ -1,3 +1,8 @@
+DROP DATABASE IF EXISTS school_management;
+CREATE DATABASE school_management;
+
+USE school_management;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -8,47 +13,31 @@ SET time_zone = "+00:00";
 
 
 CREATE TABLE `courses` (
-  `courseId` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL
+  `courseId` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  PRIMARY KEY (`courseId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `students` (
-  `studentId` int(11) NOT NULL,
+  `studentId` int(11) NOT NULL AUTO_INCREMENT,
   `firstName` varchar(50) NOT NULL,
   `lastName` varchar(50) NOT NULL,
   `gender` varchar(10) NOT NULL,
   `address` varchar(100) DEFAULT NULL,
-  `dateOfBirth` date DEFAULT NULL
+  `dateOfBirth` date DEFAULT NULL,
+  PRIMARY KEY (`studentId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `student_grades` (
   `studentId` int(11) NOT NULL,
   `courseId` int(11) NOT NULL,
-  `gradeValue` decimal(4,2) DEFAULT NULL
+  `gradeValue` decimal(4,2) DEFAULT NULL,
+  PRIMARY KEY (`studentId`,`courseId`),
+  KEY `courseId` (`courseId`),
+  CONSTRAINT `student_grades_ibfk_1` FOREIGN KEY (`studentId`) REFERENCES `students` (`studentId`) ON DELETE CASCADE,
+  CONSTRAINT `student_grades_ibfk_2` FOREIGN KEY (`courseId`) REFERENCES `courses` (`courseId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
-ALTER TABLE `courses`
-  ADD PRIMARY KEY (`courseId`);
-
-ALTER TABLE `students`
-  ADD PRIMARY KEY (`studentId`);
-
-ALTER TABLE `student_grades`
-  ADD PRIMARY KEY (`studentId`,`courseId`),
-  ADD KEY `student_grades_ibfk_2` (`courseId`);
-
-
-ALTER TABLE `courses`
-  MODIFY `courseId` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `students`
-  MODIFY `studentId` int(11) NOT NULL AUTO_INCREMENT;
-
-
-ALTER TABLE `student_grades`
-  ADD CONSTRAINT `student_grades_ibfk_1` FOREIGN KEY (`studentId`) REFERENCES `students` (`studentId`) ON DELETE CASCADE,
-  ADD CONSTRAINT `student_grades_ibfk_2` FOREIGN KEY (`courseId`) REFERENCES `courses` (`courseId`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
